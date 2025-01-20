d92ce0ab97e1e867cd9365fb0849cfa9d9e2e322
Matias Eduardo Lopez Peña <matias.lopez@udla.edu.ec>
2025-01-20 13:23:50 -0500

    Add project files.

---
+++ MVVM/Models/Note.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Models
+{
+    public class Note
+    {
+        public string Text
+        {
+            get => _note.Text;
+            set
+            {
+                if (_note.Text != value)
+                {
+                    _note.Text = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime Date => _note.Date;
+
+        public string Identifier => _note.Filename;
+    }
+}
