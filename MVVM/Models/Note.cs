b835e113c9f223ab47e7737d2f36d242bd7ea210
Matias Eduardo Lopez Peña <matias.lopez@udla.edu.ec>
2025-01-20 13:29:21 -0500

    commit 2

--- MVVM/Models/Note.cs
+++ MVVM/Models/Note.cs
@@ -1,28 +1,58 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MVVM.Models
+namespace MVVM.Models;
+
+internal class Note
 {
-    public class Note
+    public string Filename { get; set; }
+    public string Text { get; set; }
+    public DateTime Date { get; set; }
+
+    public Note()
     {
-        public string Text
-        {
-            get => _note.Text;
-            set
-            {
-                if (_note.Text != value)
-                {
-                    _note.Text = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public DateTime Date => _note.Date;
-
-        public string Identifier => _note.Filename;
+        Filename = $"{Path.GetRandomFileName()}.notes.txt";
+        Date = DateTime.Now;
+        Text = "";
     }
+
+    public void Save() =>
+    File.WriteAllText(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename), Text);
+
+    public void Delete() =>
+        File.Delete(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename));
+
+    public static Note Load(string filename)
+    {
+        filename = System.IO.Path.Combine(FileSystem.AppDataDirectory, filename);
+
+        if (!File.Exists(filename))
+            throw new FileNotFoundException("Unable to find file on local storage.", filename);
+
+        return
+            new()
+            {
+                Filename = Path.GetFileName(filename),
+                Text = File.ReadAllText(filename),
+                Date = File.GetLastWriteTime(filename)
+            };
+    }
+
+    public static IEnumerable<Note> LoadAll()
+    {
+        string appDataPath = FileSystem.AppDataDirectory;
+
+        return Directory
+
+                .EnumerateFiles(appDataPath, "*.notes.txt")
+
+                .Select(filename => Note.Load(Path.GetFileName(filename)))
+
+                .OrderByDescending(note => note.Date);
+    }
+
 }
